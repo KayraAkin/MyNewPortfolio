729adc8508c22c31f79e9b15f3430691c55dd509
kayra akın <kayrakin11@hotmail.com>
2025-04-13 17:40:43 +0300

    Add project files.

---
+++ MyNewPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyNewPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

609d105ad334e5bfb07aebe429696a68b787b9af
kayra akın <kayrakin11@hotmail.com>
2025-04-15 00:55:13 +0300

    update

---
+++ MyNewPortfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyNewPortfolio.DAL.Context;
+using MyNewPortfolio.DAL.Entities;
+
+namespace MyNewPortfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        MyPortfolioContext _context = new MyPortfolioContext();
+        public IActionResult Index()
+        {
+            var values = _context.Portfolios.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreatePortfolio()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreatePortfolio(Portfolio portfolio)
+        {
+            _context.Portfolios.Add(portfolio);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

73ebae8388551285758e9515db781836a8eda2ed
kayra akın <kayrakin11@hotmail.com>
2025-04-15 16:10:16 +0300

    update

--- MyNewPortfolio/Controllers/PortfolioController.cs
+++ MyNewPortfolio/Controllers/PortfolioController.cs
@@ -25,5 +25,28 @@ namespace MyNewPortfolio.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = _context.Portfolios.Find(id);
+            _context.Portfolios.Remove(values);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdatePortfolio(int id)
+        {
+            var values = _context.Portfolios.Find(id);
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult UpdatePortfolio(Portfolio portfolio)
+        {
+            _context.Portfolios.Update(portfolio);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
     }
 }

30768d59278779147caa8e450c76f2b39851cd84
kayra akın <kayrakin11@hotmail.com>
2025-04-15 17:14:07 +0300

    update

---
+++ MyNewPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyNewPortfolio.DAL.Context;
+using MyNewPortfolio.DAL.Entities;
+
+namespace MyNewPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyPortfolioContext _context = new MyPortfolioContext();
+        public IActionResult Index()
+        {
+            var values = _context.Abouts.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult UpdateAbout(int id)
+        {
+            var value = _context.Abouts.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateAbout(About about)
+        {
+            _context.Abouts.Update(about);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

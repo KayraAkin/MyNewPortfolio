14ccd5e36a65ed243680316f6d16f89e633beb0a
kayra akın <kayrakin11@hotmail.com>
2025-04-18 15:24:53 +0300

    update

--- MyNewPortfolio/Controllers/StatisticController.cs
+++ MyNewPortfolio/Controllers/StatisticController.cs
@@ -12,6 +12,14 @@ namespace MyNewPortfolio.Controllers
             ViewBag.v2 = _context.Messages.Count();
             ViewBag.v3 = _context.Messages.Where(x=> x.IsRead == false).Count();
             ViewBag.v4 = _context.Messages.Where(x=> x.IsRead == true).Count();
+            ViewBag.v5 = _context.Portfolios.Count();
+            ViewBag.V6 = _context.Portfolios.OrderBy(x=> x.PortfolioId).FirstOrDefault()?.Title;
+            ViewBag.v7 = _context.Experiences.Count();
+            ViewBag.v8 = _context.Experiences.OrderBy(x=> x.ExperienceId).Take(1).Select(v => v.Head + "-"+ v.Title).FirstOrDefault();
+            ViewBag.v9 = _context.Testimonials.Count();
+            ViewBag.v10 = _context.Testimonials.OrderBy(x=> x.TestimonialId).FirstOrDefault()?.NameSurname;
+            ViewBag.v11 = _context.Testimonials.OrderByDescending(x=> x.TestimonialId).FirstOrDefault()?.NameSurname;
+
             return View();
         }
     }
---
+++ MyNewPortfolio/ViewComponents/_CvComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyNewPortfolio.ViewComponents
+{
+    public class _CvComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
